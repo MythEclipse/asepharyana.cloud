98d8ed3b281e4a982c2d8d0a145dbce55d747881
Asep Haryana Saputra <90584806+MythEclipse@users.noreply.github.com>
2024-08-26 05:24:09 +0000

    cari sendiri

--- app/(project)/dashboard/layout.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="p-5">{children}</div>;
-}
--- app/(project)/product/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-export default function Layout({ children, modal }: { children: ReactNode; modal: ReactNode }) {
-  return (
-    <>
-      {children}
-      {modal}
-    </>
-  );
-}
