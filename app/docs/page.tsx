68321c5ee6e82bed25ef610289f590d3cdd5951c
Asep Haryana Saputra <90584806+MythEclipse@users.noreply.github.com>
2024-08-28 08:44:20 +0000

    cari sendiri

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -5,11 +5,5 @@ import 'swagger-ui-react/swagger-ui.css';
 import { OpenAPIV1 } from '@/app/api/docs/openapi';
 
 export default function OpenApiDocsPage() {
-  return (
-    <SwaggerUI
-      spec={OpenAPIV1}
-      displayOperationId={true}
-      
-    />
-  );
+  return <SwaggerUI spec={OpenAPIV1} displayOperationId={true} />;
 }
