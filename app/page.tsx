4635ebacfe36940d3e9ed74b749e5d4a3ecd9dbf
Asep Haryana Saputra <90584806+MythEclipse@users.noreply.github.com>
2024-08-28 07:53:20 +0000

    cari sendiri

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
 import { Metadata } from 'next';
+import Instagram from '@/components/logo/Instagram';
+import Facebook from '@/components/logo/Facebook';
+import Linkedln from '@/components/logo/Linkedln';
+import Discord from '@/components/logo/Discord';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Home'
@@ -8,6 +13,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
+  <>
     <section id="hero" className="pt-35 bg-lighta dark:bg-darkb lg:px-3 h-screen w-full">
       <div className="container">
         <div className="flex flex-wrap">
@@ -42,5 +48,53 @@ export default function Home() {
         </div>
       </div>
     </section>
+    <section id="about" className="pb-32 pt-36 bg-lighta dark:bg-darkb">
+    <div className="container">
+      <div className="flex flex-wrap">
+        <div className="mb-7 w-full px-4 lg:w-1/2">
+          <h4 className="mb-3 text-lg font-bold uppercase text-primary-600">Tentang Saya</h4>
+          <h2 className="mb-5 max-w-md text-3xl font-bold text-dark dark:text-gray-100">
+            Saya adalah seorang programmer
+          </h2>
+          <p className="max-w-xl text-base font-medium text-secondary">
+            Saya adalah seorang mahasiswa di Universitas Kuningan
+          </p>
+        </div>
+        <div className="w-full px-4 lg:w-1/2">
+          <h3 className="mb-4 text-2xl font-semibold text-dark dark:text-gray-100">Mari berteman</h3>
+          <p className="mb-6 text-base font-medium text-secondary">
+            Berikut adalah beberapa sosial media yang saya punya
+          </p>
+          <div className="flex items-center">
+            <Link
+              href="https://github.com/Asepharyana71"
+              className="item-center flex size-9 justify-center rounded-full border border-slate-300 text-dark hover:border-primary-600 hover:bg-primary-600 hover:text-white dark:text-gray-100"
+            >
+              <Discord></Discord>
+            </Link>
+            <Link
+              href="https://www.instagram.com/asepharyana18/"
+              className="item-center flex size-9 justify-center rounded-full border border-slate-300 text-dark hover:border-primary-600 hover:bg-primary-600 hover:text-white dark:text-gray-100"
+            >
+              <Instagram></Instagram>
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/asep-haryana-2014a5294/"
+              className="item-center flex size-9 justify-center rounded-full border border-slate-300 text-dark hover:border-primary-600 hover:bg-primary-600 hover:text-white dark:text-gray-100"
+            >
+              <Linkedln></Linkedln>
+            </Link>
+            <Link
+              href="https://www.facebook.com/asep.haryana.7355"
+              className="item-center flex size-9 justify-center rounded-full border border-slate-300 text-dark hover:border-primary-600 hover:bg-primary-600 hover:text-white dark:text-gray-100"
+            >
+              <Facebook></Facebook>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  </>
   );
 }
