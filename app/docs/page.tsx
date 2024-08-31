c0f8aab6078744ce08a423b4c73438df5f0d117e
Asep Haryana Saputra <90584806+MythEclipse@users.noreply.github.com>
2024-08-31 07:59:29 +0000

    cari sendiri

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,9 +1,3 @@
-'use client';
-
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
-import './custom.css';
-import OpenApiJson from '../api/docs/OpenApiJson';
 export const metadata = {
   title: {
     default: 'Dokumentasi API',
@@ -12,6 +6,12 @@ export const metadata = {
   description: 'Dokumentasi API untuk digunakan secara gratis',
   keywords: 'nextjs, api, free'
 };
+
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+import './custom.css';
+import OpenApiJson from '../api/docs/OpenApiJson';
+
 export default function OpenApiDocsPage() {
   return <SwaggerUI spec={OpenApiJson} displayOperationId={true} />;
 }
