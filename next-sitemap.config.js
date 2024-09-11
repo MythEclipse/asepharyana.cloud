6bfe36688ea8b4788bd9be54468a8ce1a1231b5a
superaseph <superaseph@gmail.com>
2024-09-11 11:16:38 +0000

    p

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.asepharyana.my.id/',
   exclude: ['/icon.svg', '/apple-icon.png', '/manifest.webmanifest', '/tags/*'],
   generateRobotsTxt: true,
   generateIndexSitemap: false,
