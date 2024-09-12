4c75836d3125e2f8095959d92e8c6383eecd136b
superaseph <superaseph@gmail.com>
2024-09-12 18:30:36 +0000

    cari sendiri

--- lib/url.ts
+++ lib/url.ts
@@ -1,4 +1,4 @@
-export const ANIMEAPI = process.env.NEXT_PUBLIC_ANIME || 'https://otakudesu-unofficial-api.vercel.app';
+export const ANIMEAPI = process.env.NEXT_PUBLIC_ANIME || 'https://otakudesu.asepharyana.my.id';
 export const Local = 'http://localhost:3090';
 export const KOMIK = process.env.NEXT_PUBLIC_KOMIK;
 export const PRODUCTION = process.env.NEXT_PUBLIC_PROD || 'https://www.asepharyana.my.id';
