02557435a6d62f71945a5bd92464d21445e62acc
root <root@asepharyana.my.id>
2024-10-05 18:58:03 +0000

    cari sendiri

--- updatebuild.sh
+++ updatebuild.sh
@@ -1,1 +1,1 @@
-git pull && pnpm install && npx prisma generate  && npm run build && pm2 restart web --update-env
+rm -rf /root/asepharyana.my.id/.next/ &&git pull && pnpm install && npx prisma generate  && npm run build && pm2 restart asepharyana.my.id --update-env
