16bc2cff28aabf5ee8a75c262deaf4088e2131ea
Asep Haryana Saputra <90584806+MythEclipse@users.noreply.github.com>
2024-11-27 19:47:34 +0000

    cari sendiri

--- app/(frontend)/docs/page.tsx
+++ app/(frontend)/docs/page.tsx
@@ -1,19 +1,25 @@
-'use client';
+export const metadata = {
+  title: {
+    default: 'Dokumentasi API',
+    template: '%s - Dokumentasi API'
+  },
+  description: 'Dokumentasi API untuk digunakan secara gratis',
+  keywords: 'nextjs, api, free'
+};
 
-import React, { useEffect, useState } from 'react';
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+import './custom.css';
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+
+const filePath = path.join(process.cwd(), 'public', 'OpenApi.yaml');
+const OpenApiYaml = fs.readFileSync(filePath, 'utf8');
+const OpenApiJson = yaml.load(OpenApiYaml);
+
+const openApiSpec = OpenApiJson as Record<string, any>;
 
 export default function OpenApiDocsPage() {
-  const [SwaggerUI, setSwaggerUI] = useState<null | React.ComponentType<any>>(null);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      import('swagger-ui-react').then((mod) => {
-        setSwaggerUI(mod.default as React.ComponentType<any>);
-      });
-    }
-  }, []);
-
-  if (!SwaggerUI) return <div>Loading...</div>;
-
-  return <SwaggerUI url="https://asepharyana.cloud/OpenApi.yaml" displayOperationId />;
+  return <SwaggerUI spec={openApiSpec} displayOperationId={true} />;
 }
