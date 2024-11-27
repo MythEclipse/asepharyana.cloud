1b4a2a57033b8574301eab6f0ed0fc411febe82c
Asep Haryana Saputra <90584806+MythEclipse@users.noreply.github.com>
2024-11-27 18:54:58 +0000

    lint

--- app/(frontend)/docs/page.tsx
+++ app/(frontend)/docs/page.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 export default function OpenApiDocsPage() {
   const [SwaggerUI, setSwaggerUI] = useState<null | React.ComponentType<any>>(null);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      import("swagger-ui-react").then((mod) => {
+      import('swagger-ui-react').then((mod) => {
         setSwaggerUI(mod.default as React.ComponentType<any>);
       });
     }
