89edbe9dee61eb5c1ade65e9668d6bb15e72b2c0
Asep Haryana Saputra <90584806+MythEclipse@users.noreply.github.com>
2024-12-04 12:04:44 +0000

    cari sendiri

--- next.config.js
+++ next.config.js
@@ -9,32 +9,28 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   swcMinify: true,
   disable: process.env.NODE_ENV === 'development',
   workboxOptions: {
-    disableDevLogs: true
+    disableDevLogs: true,
+    maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // Maksimal ukuran file 5 MB
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'runtime-cache',
+          expiration: {
+            maxEntries: 10000, // Batasi maksimal file hingga 10.000
+            maxAgeSeconds: 6 * 60 * 60 // Kedaluwarsa dalam 6 jam
+          },
+          cacheableResponse: {
+            statuses: [0, 200] // Hanya cache response dengan status 0 atau 200
+          }
+        }
+      }
+    ]
   }
-  // ... other options you like
 });
+
 const nextConfig = {
-  env: {
-    SECRET: process.env.SECRET,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-    GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
-    GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-    GEMINI_API: process.env.GEMINI_API,
-    NEXT_PUBLIC_KOMIK: process.env.NEXT_PUBLIC_KOMIK,
-    NEXT_PUBLIC_ANIME: process.env.NEXT_PUBLIC_ANIME,
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-    DATABASE_URL: process.env.DATABASE_URL
-  },
   images: {
     remotePatterns: [
       {
