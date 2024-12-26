adaf0ad15921c86a2698fe82c6894465f5ab5ed1
Asep Haryana <superaseph@gmail.com>
2024-12-26 22:41:08 +0800

    Malas isi commit message

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'asepharyana.cloud',
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production'
+      },
+      env_production: {
+        DOTENV_CONFIG_PATH: './.env'
+      }
+    }
+  ]
+};
